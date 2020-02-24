4f00c756ae3fda27950ccf13cbf1371f4c40beac
samifaith <60042337+samifaith@users.noreply.github.com>
2020-02-24 18:34:10 -0500

    Update to include Test

--- Code Wars/js/century.js
+++ Code Wars/js/century.js
@@ -1,8 +1,15 @@
-//https://www.codewars.com/kata/56dec885c54a926dcd001095
-//Opposite number
-//Very simple, given a number, find its opposite.
-//Worked with Abby
+//https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/javascript
+//Introduction
+//The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+//Task :
+//Given a year, return the century it is in.
 
-function opposite(number) {
-  return (-number)
+function century(year) {
+  return Math.ceil(year/100)
 }
+
+Test.assertEquals(century(1705), 18, 'Testing for year 1705');
+Test.assertEquals(century(1900), 19, 'Testing for year 1900');
+Test.assertEquals(century(1601), 17, 'Testing for year 1601');
+Test.assertEquals(century(2000), 20, 'Testing for year 2000');
+Test.assertEquals(century(89), 1, 'Testing for year 89');
