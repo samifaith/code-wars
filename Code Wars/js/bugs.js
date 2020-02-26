37f7c3c65b49dbf6db2cdb49f25e69aaf3807120
samifaith <60042337+samifaith@users.noreply.github.com>
2020-02-26 18:10:57 -0500

    Code Wars 2.26

---
+++ Code Wars/js/bugs.js
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/56f173a35b91399a05000cb7/train/javascript
+// Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+// There will only be one 'longest' word.
+
+function findLongest(str) {
+  var spl = str.split(" ");
+  var longest = 0;
+  for (var i = 0; i < spl.length; i++) {
+    if (spl[i].length > longest) {
+      longest = spl[i].length
+    }
+}
+  return longest
+}
+
+Test.describe("Basic tests",_=>{
+Test.assertEquals(findLongest("The quick white fox jumped around the massive dog"), 7);
+Test.assertEquals(findLongest("Take me to tinseltown with you"), 10);
+Test.assertEquals(findLongest("Sausage chops"), 7);
+Test.assertEquals(findLongest("Wind your body and wiggle your belly"), 6);
+Test.assertEquals(findLongest("Lets all go on holiday"), 7);
+})
