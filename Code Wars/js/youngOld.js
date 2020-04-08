a876d01066e03aa4644638e8459418adef1bc1fc
samifaith <60042337+samifaith@users.noreply.github.com>
2020-04-08 14:51:39 -0400

    updated prep

--- Code Wars/js/youngOld.js
+++ Code Wars/js/youngOld.js
@@ -9,22 +9,43 @@
 // ‘age’ of 0. Return a new array with [youngest age, oldest age, difference between
 // the youngest and oldest age].
 
-function differenceInAges(ages){
- let maxAge = Math.max.apply(null, ages)
- let minAge = Math.min.apply(null, ages)
- let diff = maxAge - minAge
- let newArr = [minAge, maxAge, diff]
+// P: [Ages]
+// R: Min Age, Max Age, Difference of the Ages in a newArr
+// E: [2,18,33,4,99,12] -> [2,99,97]
+// P: maxAge = Math.max.apply(arr)
+// -  minAge = Math.min.apply(arr)
+// -  diffAge = maxAge - minAge
+// -  newArr = [minAge, maxAge, diffAge]
 
- return newArr
+let ages = [12,2,16,99,4]
+function diffInAges(ages){
+  let maxAge = Math.max(...ages);
+  let minAge = Math.min(...ages);
+  let ageDiff = maxAge - minAge;
+  let newArr = [minAge, maxAge, ageDiff];
+   console.log(newArr)
 }
 
-Test.assertSimilar(differenceInAges([16, 22, 31, 44, 3, 38, 27, 41, 88]), [3, 88, 85]);
-Test.assertSimilar(differenceInAges([5, 8, 72, 98, 41, 16, 55]), [5, 98, 93]);
-Test.assertSimilar(differenceInAges([57, 99, 14, 32]), [14, 99, 85]);
-Test.assertSimilar(differenceInAges([62, 0, 3, 77, 88, 102, 26, 44, 55]), [0, 102, 102]);
-Test.assertSimilar(differenceInAges([2, 44, 34, 67, 88, 76, 31, 67]), [2, 88, 86]);
-Test.assertSimilar(differenceInAges([46, 86, 33, 29, 87, 47, 28, 12, 1, 4, 78, 92]), [1, 92, 91]);
-Test.assertSimilar(differenceInAges([66, 73, 88, 24, 36, 65, 5]), [5, 88  , 83]);
-Test.assertSimilar(differenceInAges([12, 76, 49, 37, 29, 17, 3, 65, 84, 38]), [3, 84, 81]);
-Test.assertSimilar(differenceInAges([0, 110]), [0, 110, 110]);
-Test.assertSimilar(differenceInAges([33, 33, 33]), [33, 33, 0]);
+diffInAges(ages)
+
+// ******* Original code war below - practice with prep above **********
+
+// function differenceInAges(ages){
+//  let maxAge = Math.max.apply(null, ages)
+//  let minAge = Math.min.apply(null, ages)
+//  let diff = maxAge - minAge
+//  let newArr = [minAge, maxAge, diff]
+//
+//  return newArr
+// }
+// 
+// Test.assertSimilar(differenceInAges([16, 22, 31, 44, 3, 38, 27, 41, 88]), [3, 88, 85]);
+// Test.assertSimilar(differenceInAges([5, 8, 72, 98, 41, 16, 55]), [5, 98, 93]);
+// Test.assertSimilar(differenceInAges([57, 99, 14, 32]), [14, 99, 85]);
+// Test.assertSimilar(differenceInAges([62, 0, 3, 77, 88, 102, 26, 44, 55]), [0, 102, 102]);
+// Test.assertSimilar(differenceInAges([2, 44, 34, 67, 88, 76, 31, 67]), [2, 88, 86]);
+// Test.assertSimilar(differenceInAges([46, 86, 33, 29, 87, 47, 28, 12, 1, 4, 78, 92]), [1, 92, 91]);
+// Test.assertSimilar(differenceInAges([66, 73, 88, 24, 36, 65, 5]), [5, 88  , 83]);
+// Test.assertSimilar(differenceInAges([12, 76, 49, 37, 29, 17, 3, 65, 84, 38]), [3, 84, 81]);
+// Test.assertSimilar(differenceInAges([0, 110]), [0, 110, 110]);
+// Test.assertSimilar(differenceInAges([33, 33, 33]), [33, 33, 0]);
--- Code Wars/js/zerohero.js
+++ Code Wars/js/zerohero.js
@@ -10,7 +10,31 @@
 
 
 function noBoringZeros(n) {
-  while(n != 0 && n % 10 === 0)
+  while(n != 0 && n % 10 === 0){
     n /= 10;
-  return n;
+  } return n;
+}
+
+
+// They might be fun in your world, but not here.
+// Get rid of them. Only the ending ones.
+// P: Enter in a number and reduce all zeros at the end of the number
+// R: Return the number without zeros at the end
+// E: 100100 -> 1001
+// P: run function(num)
+// -  while the num != 0 && num is % 10
+// -  num/10
+// - return num
+// 1450 -> 145
+// 960000 -> 96
+// 1050 -> 105
+// -1050 -> -105
+// Zero alone is fine, don't worry about it. Poor guy anyway
+
+
+function returnZeroZeros (num){
+  while (num != 0 && num % 10 === 0){
+    num /= 10
+  }
+  console.log(num)
 }
